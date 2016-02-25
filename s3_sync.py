770f4755c79d41fea1f001a4f53f18f6bc12c970
William Jimenez <wjimenez5271@gmail.com>
2016-02-24 18:42:04 -0800

    add options for argparse

--- s3_sync.py
+++ s3_sync.py
@@ -1,5 +1,8 @@
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection
+import logging
+
+logger = logging.getlogger(__name__)
 
 bucket_name = 'ignite_chat_data'
 conn = S3Connection()
@@ -10,17 +13,25 @@ k.key = 'db_json'
 
 
 def get_data(json_file):
+    logger.info('Fetching data from S3')
     with open(json_file, 'rb') as f:
         k.get_contents_to_file(f)
 
 
 def put_data(json_file):
+    logginger.info('Storing data to S3')
     k.set_contents_from_file(json_file)
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--get_data', action='store_true')
+    parser.add_argument('--store_data', action='store_true')
+    parser.add_argument('--json_path', default='db.json')
     args = parser.parse_args()
+    
     if args.get_data:
-        get_data('db.json')
+        get_data(arg.json_path)
+    elif args.store_data:
+        put_data(args.json_path)
